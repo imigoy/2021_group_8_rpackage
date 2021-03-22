a454dad2d57dab87449962ff7f29109fc336fd27
theis2 <patricktheisjensen@hotmail.com>
2021-03-22 10:13:25 +0100

    A few changes to the stolen function

--- R/Random_DNA.R
+++ R/Random_DNA.R
@@ -2,7 +2,11 @@
 #Testing out the random DNA function to get an idea of what we are doing.
 # R/random_dna.R
 random_dna <- function(l){
-  nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
+  nucleotides <- sample(c("A", "T", "G", "C"), size = 99, replace = TRUE) 
   dna = paste0(nucleotides, collapse = "")
   return(dna)
 }
+
+#This gives a random DNA string of 99 basepairs
+
+
